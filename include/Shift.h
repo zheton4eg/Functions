e1aa6f4f7ba781b7db4fe9afc434c3f903179d69
zheka <zheka@HOME-PC>
2024-05-21 17:28:35 +0300

    LIB_DONE

---
+++ include/Shift.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Constants.h"
+void ShiftLeft(int arr[], const int n, int shiftsL = 0);
+void ShiftLeft(double arr[], const int n, int shiftsL = 0);
+void ShiftLeft(char arr[], const int n, int shiftsL = 0);
+
+void ShiftRight(int arr[], const int n, int shiftsR = 0);
+void ShiftRight(double arr[], const int n, int shiftsR = 0);
+void ShiftRight(char arr[], const int n, int shiftsR = 0);
